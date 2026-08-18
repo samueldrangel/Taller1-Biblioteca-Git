c9dd138396007f31a6a6841892c49f2da4c21964
Samuel David Rangel Martinez <samueldrangel@unicesar.edu.co>
2026-08-18 11:06:35 -0500

    feat: crear clase Persona como padre de Cliente

--- src/main/java/com/mycompany/taller1/biblioteca/git/Persona.java
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Persona.java
@@ -9,5 +9,42 @@ package com.mycompany.taller1.biblioteca.git;
  * @author PC
  */
 public class Persona {
+    private String id;
+    private String nombre;
+    private String telefono;
+
     
+    public Persona() {
+    }
+
+    
+    public Persona(String id, String nombre, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
