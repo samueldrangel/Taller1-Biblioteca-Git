2c0f82b002da20e83953977f6a3f12303a6d70ef
Samuel David Rangel Martinez <samueldrangel@unicesar.edu.co>
2026-08-20 17:06:43 -0500

    feat: implementar CREATE de Libro

--- src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
@@ -13,6 +13,8 @@ import java.util.Scanner;
 public class Main {
 
     static ArrayList<Cliente> clientes = new ArrayList<>();
+    static ArrayList<Libro> libros = new ArrayList<>();
+
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -95,5 +97,28 @@ public class Main {
             }
         }
         System.out.println("Cliente no encontrado.");
+    }
+        
+    public static void crearLibro() {
+
+        System.out.println("\n--- CREAR LIBRO ---");
+
+        System.out.print("Código: ");
+        String codigo = sc.nextLine();
+
+        System.out.print("Título: ");
+        String titulo = sc.nextLine();
+
+        System.out.print("Autor: ");
+        String autor = sc.nextLine();
+
+        System.out.print("Cantidad de páginas: ");
+        int cantidadPaginas = Integer.parseInt(sc.nextLine());
+
+        Libro libro = new Libro(codigo, titulo, autor, cantidadPaginas);
+
+        libros.add(libro);
+
+        System.out.println("Libro creado correctamente.");
     }
 }
