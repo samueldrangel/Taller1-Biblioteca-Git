b1da7f1d99ea14dc54384d7efe10d84ca9f070f5
Samuel David Rangel Martinez <samueldrangel@unicesar.edu.co>
2026-08-20 17:37:57 -0500

    feat: implementar READ de Libro (buscar)

--- src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
@@ -135,5 +135,17 @@ public class Main {
             System.out.println(libro);
         }
     }
+    
+        public static Libro buscarLibro(String codigo) {
+
+        for (Libro libro : libros) {
+
+            if (libro.getCodigo().equalsIgnoreCase(codigo)) {
+                return libro;
+            }
+        }
+
+        return null;
+    }
 
 }
