a92a909918f67513ed2390db478e6ae8c4df2688
Samuel David Rangel Martinez <samueldrangel@unicesar.edu.co>
2026-08-20 17:50:35 -0500

    feat: crear clase Prestamo que relaciona Cliente y Libro

--- src/main/java/com/mycompany/taller1/biblioteca/git/Prestamo.java
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Prestamo.java
@@ -9,5 +9,45 @@ package com.mycompany.taller1.biblioteca.git;
  * @author PC
  */
 public class Prestamo {
-    
+ 
+    private Cliente cliente;
+    private Libro libro;
+    private boolean activo;
+ 
+    public Prestamo(Cliente cliente, Libro libro) {
+        this.cliente = cliente;
+        this.libro = libro;
+        this.activo = true;
+    }
+ 
+    public Cliente getCliente() {
+        return cliente;
+    }
+ 
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+ 
+    public Libro getLibro() {
+        return libro;
+    }
+ 
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+ 
+    public boolean isActivo() {
+        return activo;
+    }
+ 
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+ 
+    @Override
+    public String toString() {
+        return "Cliente: " + cliente
+                + ", Libro: " + libro
+                + ", Estado: " + (activo ? "Activo" : "Devuelto");
+    }
 }
